d24f258984c156b457b561dec67b3f13d803860b
David PR <serallapdivad@gmail.com>
2023-11-18 15:19:45 +0000

    All the HTML content copied to the blade.php

---
+++ views/layout.blade.php
@@ -0,0 +1,220 @@
+<header-container>
+    <div class="lock"><h3>We Make Your Feel Comfortable</h3></div>
+    <header class="header">
+        <div class="header__mobile-left-items">
+            <div class="header__mobile-icons-left">
+                <div
+                    class="material-symbols-outlined header__mobile-burger-icon"
+                >
+                    menu
+                </div>
+                <div
+                    class="material-symbols-outlined header__mobile-close-icon"
+                >
+                    close
+                </div>
+            </div>
+            <div class="header__logo-image">
+                <div>
+                    <a href="./index.html">
+                        <img
+                            class="header__logo-image-h"
+                            src="./assets/images/miranda_h_logo.png"
+                            alt="an image of the logo"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <a href="./index.html">
+                        <img
+                            class="header__logo-image-words"
+                            src="./assets/images/miranda_words_logo.png"
+                            alt="an image of the logo"
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header__desktop-menu">
+            <ul class="header__desktop-menu-list">
+                <li class="header__desktop-menu-list-item">
+                    <a href="./aboutUs.html">About Us</a>
+                </li>
+                <li class="header__desktop-menu-list-item">
+                    <a href="./rooms.html">Rooms</a>
+                </li>
+                <li class="header__desktop-menu-list-item">
+                    <a href="./offers.html">Offers</a>
+                </li>
+                <li class="header__desktop-menu-list-item">
+                    <a href="./contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <div class="header__mobile-icons-right">
+            <div
+                class="header__desktop-burger-icon material-symbols-outlined"
+            >
+                menu
+            </div>
+            <div class="header__search-icon material-symbols-outlined">
+                search
+            </div>
+            <div class="header__person-icon material-symbols-outlined">
+                person
+            </div>
+        </div>
+    </header>
+    <section id="dropDown" class="mobile__drop-down-menu">
+        <ul class="mobile__menu-list">
+            <li class="mobile__menu-list-item">
+                <a href="./aboutUs.html">About Us</a>
+            </li>
+            <li class="mobile__menu-list-item">
+                <a href="./rooms.html">Rooms</a>
+            </li>
+            <li class="mobile__menu-list-item">
+                <a href="./offers.html">Offers</a>
+            </li>
+            <li class="mobile__menu-list-item">
+                <a href="./contact.html">Contact</a>
+            </li>
+        </ul>
+    </section>
+</header-container>
+
+<footer-container>
+    <main class="footer">
+        <div class="footer__external-container">
+            <div class="footer__inner-container">
+                <img
+                    class="footer__logo"
+                    src="./assets/images/miranda_logo_golden.png"
+                    alt="a golden miranda hotel logo"
+                />
+                <p class="footer__info-text">
+                    Discover comfort and luxury at Miranda Hotel. Experience
+                    impeccable service, stunning accommodations, and
+                    unforgettable moments. Your perfect stay awaits. Book
+                    now for an extraordinary experience.
+                </p>
+                <div class="footer__social-links">
+                    <a href="https://www.facebook.com/"
+                        ><img
+                            id="social-fb"
+                            src="./assets/images/social_btn/fb_social_white.png"
+                            alt=""
+                    /></a>
+                    <a href="https://twitter.com/"
+                        ><img
+                            id="social-tw"
+                            src="./assets/images/social_btn/tw_social_white.png"
+                            alt=""
+                    /></a>
+                    <a href="https://www.behance.net/"
+                        ><img
+                            id="social-be"
+                            src="./assets/images/social_btn/be_social_white.png"
+                            alt=""
+                    /></a>
+                    <a href="https://www.linkedin.com/"
+                        ><img
+                            id="social-in"
+                            src="./assets/images/social_btn/in_social_white.png"
+                            alt=""
+                    /></a>
+                    <a href="https://www.youtube.com/"
+                        ><img
+                            id="social-yt"
+                            src="./assets/images/social_btn/yt_social_white.png"
+                            alt=""
+                    /></a>
+                </div>
+            </div>
+            <div>
+                <h4 class="footer__services-title small-title">
+                    Services.
+                </h4>
+                <div class="footer__services">
+                    <div class="footer__services-colum-left">
+                        <span class="footer__services-element"
+                            >+ Restaurant & Bar</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Swimming Pool</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Wellness & Spa</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Restaurant</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Conference Room</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Coctail Party House</span
+                        >
+                    </div>
+                    <div class="footer__services-colum-right">
+                        <span class="footer__services-element"
+                            >+ Gaming Zone</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Marrige Party</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Party Planning</span
+                        >
+                        <span class="footer__services-element"
+                            >+ Tour Consultancy</span
+                        >
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h4 class="footer__contact-us-title small-title">
+                    Contact Us.
+                </h4>
+                <div class="footer__contact-us">
+                    <div class="footer__contact-us-element">
+                        <img
+                            src="./assets/images/contact_icons/call_icon.png"
+                            alt=""
+                        />
+                        <div class="footer__contact-us-text-element">
+                            <h4>Phone Number</h4>
+                            <p>+34 934 764 263</p>
+                        </div>
+                    </div>
+                    <div class="footer__contact-us-element">
+                        <img
+                            src="./assets/images/contact_icons/email_icon.png"
+                            alt=""
+                        />
+                        <div class="footer__contact-us-text-element">
+                            <h4>Email Address</h4>
+                            <p>miranda@hotels.luxury</p>
+                        </div>
+                    </div>
+                    <div class="footer__contact-us-element">
+                        <img
+                            src="./assets/images/contact_icons/address_icon.png"
+                            alt=""
+                        />
+                        <div class="footer__contact-us-text-element">
+                            <h4>Current Location</h4>
+                            <p>C/ Sinfiol 146, CP: OX1-1AA</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <div class="copyright">
+        <div class="copyright__inner-content">
+            <p>© 2023 Miranda Hotels</p>
+            <span>Terms of use | Privacy Environmental Policy</span>
+        </div>
+    </div>
+</footer-container>
