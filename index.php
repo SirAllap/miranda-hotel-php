e3932deecd0203994da74e95333b6e6cf7e1d2f1
David PR <serallapdivad@gmail.com>
2023-11-20 10:39:05 +0000

    Testing sql query on index.php

--- index.php
+++ index.php
@@ -1,28 +1,15 @@
 <?php
-require 'lib/BladeOne.php';
-require __DIR__ . '/vendor/autoload.php';
-Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/')->load();
-
-include("config.php");
+//BladeTemplate
+require_once('setup.php');
+//DB config
+require_once('config.php');
 
 $sql = "SELECT id, room_number FROM room";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while ($columns = $result->fetch_assoc()) {
-        echo "ID: " . $columns["id"] . " - Room Number: " . $columns["room_number"] . "\n";
-    }
-} else {
-    echo "0 results";
-}
+$data = $result->fetch_all(MYSQLI_ASSOC);
+
+// print_r($data);
+
+echo $blade->run('index', ['data' => $data]);
 $conn->close();
-
-use eftec\bladeone\BladeOne;
-
-$views = 'views'; // Specify the directory where your Blade templates are located.
-$compiledFolder = 'cache'; // Specify the directory where compiled views will be stored.
-
-$blade = new BladeOne($views, $compiledFolder, BladeOne::MODE_AUTO);
-
-// Example usage
-echo $blade->run('index');
