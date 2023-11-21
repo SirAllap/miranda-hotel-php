307261b96429d3d2e33c232a0f0070aa2bb68f4c
David PR <serallapdivad@gmail.com>
2023-11-21 17:30:24 +0000

    rooms querye: modified the sql query to always show a default image of the rooms

--- rooms.php
+++ rooms.php
@@ -4,10 +4,10 @@ require_once('setup.php');
 //DB config
 require_once('config.php');
 
-$sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id;";
+$sql = "SELECT r.*, COALESCE(p.URL, 'https://picsum.photos/seed/WsPyhTrC/640/480') AS URL FROM room r LEFT JOIN photo p ON r.id = p.room_id;";
 $result = $conn->query($sql);
 
 $rooms = $result->fetch_all(MYSQLI_ASSOC);
 
-echo $blade->run('rooms');
+echo $blade->run('rooms', ['rooms' => $rooms]);
 $conn->close();
