5763dc5b79b1cbb78914ba1b01ee2031de7c666a
David PR <serallapdivad@gmail.com>
2023-11-21 10:00:57 +0000

    Improved sql query to bring the room photo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('setup.php');
 //DB config
 require_once('config.php');
 
-$sql = "SELECT * FROM room";
+$sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id;";
 $result = $conn->query($sql);
 
 $rooms = $result->fetch_all(MYSQLI_ASSOC);
