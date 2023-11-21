21510c08a7037a4f4469a5674c46c1de3996bbce
David PR <serallapdivad@gmail.com>
2023-11-21 21:37:59 +0000

    Form contact now create a new contact review on db

--- contact.php
+++ contact.php
@@ -4,10 +4,32 @@ require_once('setup.php');
 //DB config
 require_once('config.php');
 
-$sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id;";
-$result = $conn->query($sql);
+$confirmation = false;
 
-$rooms = $result->fetch_all(MYSQLI_ASSOC);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["phone"]) && isset($_POST["subject"]) && isset($_POST["message"])) {
+        $full_name = htmlspecialchars($_POST["name"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $phone_number = htmlspecialchars($_POST["phone"]);
+        $subject_of_review = htmlspecialchars($_POST["subject"]);
+        $review_body = htmlspecialchars($_POST["message"]);
 
-echo $blade->run('contact');
+        $sql = "INSERT INTO contact (full_name, email, phone_number, subject_of_review, review_body, date, status) VALUES (?, ?, ?, ?, ?, CURDATE(), true)";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sssss", $full_name, $email, $phone_number, $subject_of_review, $review_body);
+
+        if ($stmt->execute()) {
+            $confirmation = true;
+        } else {
+            $confirmation = "Error: " . $stmt->error;
+        }
+
+        $stmt->close();
+    } else {
+        $confirmation = "Form fields are not set.";
+    }
+}
+
+echo $blade->run('contact', ['confirmation' => $confirmation]);
 $conn->close();
