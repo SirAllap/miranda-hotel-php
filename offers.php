3870f520be1849a00225370fc5e5f9baa6613851
David PR <serallapdivad@gmail.com>
2023-11-22 21:47:13 +0000

    Generated SQL query to get all room with discount

--- offers.php
+++ offers.php
@@ -4,10 +4,11 @@ require_once('setup.php');
 //DB config
 require_once('config.php');
 
-$sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id;";
+$sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id WHERE r.discount > 0 LIMIT 5;";
+
 $result = $conn->query($sql);
 
 $rooms = $result->fetch_all(MYSQLI_ASSOC);
 
-echo $blade->run('offers');
+echo $blade->run('offers', ['rooms' => $rooms]);
 $conn->close();
