3092134f20601c484ff1cae4134f3bd59347f147
David PR <serallapdivad@gmail.com>
2023-11-22 15:14:46 +0000

    SQL query will retrieve the room by ID

--- room-details.php
+++ room-details.php
@@ -4,10 +4,16 @@ require_once('setup.php');
 //DB config
 require_once('config.php');
 
-$sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id;";
+if (isset($_GET["room_id"])) {
+    $id = htmlspecialchars($_GET["room_id"]);
+
+    $sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id WHERE r.id = $id;";
+}
+
 $result = $conn->query($sql);
 
-$rooms = $result->fetch_all(MYSQLI_ASSOC);
+$room = $result->fetch_assoc();
 
-echo $blade->run('room-details');
+echo $blade->run('room-details', ['room' => $room]);
+
 $conn->close();
