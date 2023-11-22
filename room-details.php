5460439bcc6fbcd0617177410a65c77ce3ffaf3f
David PR <serallapdivad@gmail.com>
2023-11-22 18:22:51 +0000

    Room-details: Read the session variables with dates and pass them to the HTML

--- room-details.php
+++ room-details.php
@@ -1,10 +1,13 @@
 <?php
+session_start();
 //BladeTemplate
 require_once('setup.php');
 //DB config
 require_once('config.php');
 
 if (isset($_GET["room_id"])) {
+    isset($_SESSION['start']) ? $start = $_SESSION['start'] : $start = null;
+    isset($_SESSION['end']) ? $end = $_SESSION['end'] : $end = null;
     $id = htmlspecialchars($_GET["room_id"]);
 
     $sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id WHERE r.id = $id;";
@@ -14,6 +17,7 @@ $result = $conn->query($sql);
 
 $room = $result->fetch_assoc();
 
-echo $blade->run('room-details', ['room' => $room]);
+echo $blade->run('room-details', ['room' => $room, 'start' => $start, 'end' => $end]);
 
 $conn->close();
+session_destroy();
