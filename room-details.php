731a5b357296097b64b546b2c29006b89c48b363
David PR <serallapdivad@gmail.com>
2023-11-23 19:35:21 +0000

    Improved sql query to also get all amenities

--- room-details.php
+++ room-details.php
@@ -13,23 +13,34 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
         $trip_start = htmlspecialchars($_GET["trip-start"]);
         $trip_end = htmlspecialchars($_GET["trip-end"]);
 
-        $sql = "SELECT r.*, p.URL, b.check_in, b.check_out
-        FROM room r
-        INNER JOIN photo p ON r.id = p.room_id
-        INNER JOIN booking b ON r.id = b.room_id
-        WHERE r.id = $id
-        AND NOT EXISTS (
-            SELECT 1
-            FROM booking b_sub
-            WHERE r.id = b_sub.room_id
-            AND (
-                ('$trip_start' BETWEEN b_sub.check_in AND b_sub.check_out)
-                OR ('$trip_end' BETWEEN b_sub.check_in AND b_sub.check_out)
-                OR (b_sub.check_in BETWEEN '$trip_start' AND '$trip_end')
-                OR (b_sub.check_out BETWEEN '$trip_start' AND '$trip_end')
-            )
-        );
-        ";
+        $sql = "SELECT
+                r.*,
+                p.URL,
+                b.check_in,
+                b.check_out,
+                GROUP_CONCAT(a.amenities SEPARATOR ', ') AS all_amenities
+                FROM
+                    room r
+                    INNER JOIN photo p ON r.id = p.room_id
+                    LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id
+                    LEFT JOIN amenity a ON ahr.amenity_id = a.id
+                    INNER JOIN booking b ON r.id = b.room_id
+                WHERE
+                    r.id = $id
+                GROUP BY
+                    r.id, p.URL, b.check_in, b.check_out
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM booking b_sub
+                    WHERE r.id = b_sub.room_id
+                    AND (
+                        ('$trip_start' BETWEEN b_sub.check_in AND b_sub.check_out)
+                        OR ('$trip_end' BETWEEN b_sub.check_in AND b_sub.check_out)
+                        OR (b_sub.check_in BETWEEN '$trip_start' AND '$trip_end')
+                        OR (b_sub.check_out BETWEEN '$trip_start' AND '$trip_end')
+                    )
+                );
+                ";
 
         $sqlRelatedRooms = "SELECT r.*, p.URL FROM room r LEFT JOIN photo p ON r.id = p.room_id WHERE r.status = true AND r.discount = 0 LIMIT 10;";
 
@@ -45,7 +56,23 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
         isset($_SESSION['end']) ? $end = $_SESSION['end'] : $end = null;
         $id = htmlspecialchars($_GET["room_id"]);
         $_SESSION['room_id'] = $id;
-        $sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id WHERE r.id = $id;";
+        $sql = "SELECT
+                r.*,
+                p.URL,
+                b.check_in,
+                b.check_out,
+                GROUP_CONCAT(a.amenities SEPARATOR ', ') AS all_amenities
+                FROM
+                    room r
+                    INNER JOIN photo p ON r.id = p.room_id
+                    LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id
+                    LEFT JOIN amenity a ON ahr.amenity_id = a.id
+                    INNER JOIN booking b ON r.id = b.room_id
+                WHERE
+                    r.id = $id
+                GROUP BY
+                    r.id, p.URL, b.check_in, b.check_out;";
+
         $sqlRelatedRooms = "SELECT r.*, p.URL FROM room r LEFT JOIN photo p ON r.id = p.room_id WHERE r.status = true AND r.discount = 0 LIMIT 10;";
         $result = $conn->query($sql);
         $room = $result->fetch_assoc();
