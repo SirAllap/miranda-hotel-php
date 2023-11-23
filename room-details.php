0252398d741983715072e9374c37eb0308e6878d
David PR <serallapdivad@gmail.com>
2023-11-23 17:51:18 +0000

    Room details:
    Added a new condition to check if the selected room is available within a certain date range

--- room-details.php
+++ room-details.php
@@ -5,24 +5,56 @@ require_once('setup.php');
 //DB config
 require_once('config.php');
 
+
+
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    if (isset($_GET["room_id"])) {
+    if (isset($_GET["room_id"]) && isset($_GET["trip-start"]) && isset($_GET["trip-end"])) {
+        $id = htmlspecialchars($_GET["room_id"]);
+        $trip_start = htmlspecialchars($_GET["trip-start"]);
+        $trip_end = htmlspecialchars($_GET["trip-end"]);
+
+        $sql = "SELECT r.*, p.URL, b.check_in, b.check_out
+        FROM room r
+        INNER JOIN photo p ON r.id = p.room_id
+        INNER JOIN booking b ON r.id = b.room_id
+        WHERE r.id = 46
+        AND NOT EXISTS (
+            SELECT 1
+            FROM booking b_sub
+            WHERE r.id = b_sub.room_id
+            AND (
+                ('$trip_start' BETWEEN b_sub.check_in AND b_sub.check_out)
+                OR ('$trip_end' BETWEEN b_sub.check_in AND b_sub.check_out)
+                OR (b_sub.check_in BETWEEN '$trip_start' AND '$trip_end')
+                OR (b_sub.check_out BETWEEN '$trip_start' AND '$trip_end')
+            )
+        );
+        ";
+
+        $sqlRelatedRooms = "SELECT r.*, p.URL FROM room r LEFT JOIN photo p ON r.id = p.room_id WHERE r.status = true AND r.discount = 0 LIMIT 10;";
+
+        $result = $conn->query($sql);
+        $room = $result->fetch_assoc();
+
+        $resultRelatedRooms = $conn->query($sqlRelatedRooms);
+        $rooms = $resultRelatedRooms->fetch_all(MYSQLI_ASSOC);
+
+        echo $blade->run('room-details', ['room' => $room, 'rooms' => $rooms, 'start' => $trip_start, 'end' => $trip_end]);
+    } else if (isset($_GET["room_id"])) {
         isset($_SESSION['start']) ? $start = $_SESSION['start'] : $start = null;
         isset($_SESSION['end']) ? $end = $_SESSION['end'] : $end = null;
         $id = htmlspecialchars($_GET["room_id"]);
         $_SESSION['room_id'] = $id;
-
         $sql = "SELECT r.*, p.URL FROM room r INNER JOIN photo p ON r.id = p.room_id WHERE r.id = $id;";
-
         $sqlRelatedRooms = "SELECT r.*, p.URL FROM room r LEFT JOIN photo p ON r.id = p.room_id WHERE r.status = true AND r.discount = 0 LIMIT 10;";
+        $result = $conn->query($sql);
+        $room = $result->fetch_assoc();
+        $resultRelatedRooms = $conn->query($sqlRelatedRooms);
+        $rooms = $resultRelatedRooms->fetch_all(MYSQLI_ASSOC);
+        echo $blade->run('room-details', ['room' => $room, 'rooms' => $rooms, 'start' => $start, 'end' => $end]);
+    } else {
+        echo $blade->run('index', ['confirmation' =>  'Something went wrong, please try again!']);
     }
-    $result = $conn->query($sql);
-    $room = $result->fetch_assoc();
-
-    $resultRelatedRooms = $conn->query($sqlRelatedRooms);
-    $rooms = $resultRelatedRooms->fetch_all(MYSQLI_ASSOC);
-
-    echo $blade->run('room-details', ['room' => $room, 'rooms' => $rooms, 'start' => $start, 'end' => $end]);
 } else if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (isset($_POST["trip-start"]) && isset($_POST["trip-end"]) && isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["phone"]) && isset($_POST["special-request"])) {
         $trip_start = htmlspecialchars($_POST["trip-start"]);
@@ -39,9 +71,10 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
         $stmt->bind_param("sssssss", $full_name, $phone_number, $email, $trip_start, $trip_end, $special_request, $room_id);
 
         if ($stmt->execute()) {
-            $confirmation = 'Thank you for your request. We have received it correctly. Someone from our Team will get back to you very soon.';
-            echo $blade->run('index', ['confirmation' =>  $confirmation]);
             session_destroy();
+            $confirmation = 'Thank you for your request. We have received it correctly. Someone from our Team will get back to you very soon.';
+            header('Refresh: 3; "index.php"');
+            echo $blade->run('index', ['confirmation' =>  $confirmation]);
         } else {
             $confirmation = 'Form not sent! | Error: ' . $stmt->error;
         }
