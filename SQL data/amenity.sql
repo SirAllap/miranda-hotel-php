f3a6f1d02d79a507c5f35a86e9483cf7741fb0a7
David PR <serallapdivad@gmail.com>
2023-11-27 11:47:36 +0000

    added db data

---
+++ SQL data/amenity.sql
@@ -0,0 +1,15 @@
+INSERT INTO travl_db.amenity (amenities) VALUES
+	 ('Free Wifi'),
+	 ('Towels'),
+	 ('Expert Team'),
+	 ('Single Bed'),
+	 ('Kitchen'),
+	 ('Air Conditioner'),
+	 ('Strong Locker'),
+	 ('Breakfast'),
+	 ('Cleaning'),
+	 ('Grocery');
+INSERT INTO travl_db.amenity (amenities) VALUES
+	 ('Shop Near'),
+	 ('24/7 Online Support'),
+	 ('Smart Security');
